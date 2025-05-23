5ab181952b07e668da60fb1d12d294d8d423f15e
kami <kami-vi@mail.ru>
2025-05-23 16:13:01 +0400

    feat: implement signup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,84 @@
-import './App.css'
-import { Signin } from './components/sign-in'
+import styled from 'styled-components'
 import { Signup } from './components/sign-up'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
-const App = () => {
+interface AppContainerProps {
+	className?: string
+}
+
+const signupSchema = yup.object({
+	name: yup
+		.string()
+		.required('Обязательное поле')
+		.min(2, 'Минимум 2 символа')
+		.max(50, 'Максимум 50 символов')
+		.matches(/^[a-zA-Zа-яА-ЯёЁ\s\-]+$/, 'Только буквы, пробелы и дефисы'),
+
+	nickname: yup
+		.string()
+		.required('Обязательное поле')
+		.min(3, 'Минимум 3 символа')
+		.max(20, 'Максимум 20 символов')
+		.matches(
+			/^[a-zA-Z0-9_]+$/,
+			'Только латинские буквы, цифры и подчёркивание'
+		),
+
+	email: yup
+		.string()
+		.required('Обязательное поле')
+		.email('Некорректный email')
+		.max(100, 'Максимум 100 символов')
+		.matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Некорректный формат email'),
+
+	gender: yup
+		.string()
+		.required('Выберите пол')
+		.oneOf(['male', 'female'], 'Некорректное значение'),
+
+	password: yup
+		.string()
+		.required('Обязательное поле')
+		.min(6, 'Минимум 6 символов')
+		.max(50, 'Максимум 50 символов')
+		.matches(
+			/^[\w!@#$%^&*()\-+=~`[\]{}|:;"'<>,.?/]+$/,
+			'Пароль содержит недопустимые символы'
+		),
+
+	confirmPassword: yup
+		.string()
+		.required('Подтвердите пароль')
+		.oneOf([yup.ref('password')], 'Пароли не совпадают'),
+})
+
+type FormValues = yup.InferType<typeof signupSchema>
+
+const AppContainer: React.FC<AppContainerProps> = ({ className }) => {
+	const {
+		register,
+		formState: { errors },
+		handleSubmit,
+	} = useForm<FormValues>({
+		resolver: yupResolver(signupSchema),
+	})
+	const onSubmit = (data: FormValues) => {
+		console.log('Форма отправлена:', data)
+	}
 	return (
-		<div className='App'>
-			<Signin />
-			<Signup />
+		<div className={className}>
+			<Signup
+				register={register}
+				errors={errors}
+				onSubmit={handleSubmit(onSubmit)}
+			/>
 		</div>
 	)
 }
 
-export default App
+export const App = styled(AppContainer)`
+	display: flex;
+	justify-content: center;
+`
--- src/components/sign-up.tsx
+++ src/components/sign-up.tsx
@@ -1,14 +1,111 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Input } from './input'
+import { UseFormRegister, FieldErrors } from 'react-hook-form'
 
-interface SignupContainerProps {
-	className?: string
+interface FormValues {
+	name: string
+	nickname: string
+	email: string
+	gender: string
+	password: string
+	confirmPassword: string
 }
 
-const SignupContainer: React.FC<SignupContainerProps> = ({ className }) => (
-	<div className={className}>
-		<div></div>
-	</div>
-)
+interface SignupProps {
+	className?: string
+	register: UseFormRegister<FormValues>
+	errors: FieldErrors<FormValues>
+	onSubmit: React.FormEventHandler<HTMLFormElement>
+}
 
-export const Signup = styled(SignupContainer)``
+const SignupContainer: React.FC<SignupProps> = ({
+	className,
+	register,
+	errors,
+	onSubmit,
+}) => {
+	return (
+		<div className={className}>
+			<form onSubmit={onSubmit}>
+				<Input
+					label='Имя'
+					type='text'
+					placeholder='Введите имя'
+					reg={{ ...register('name') }}
+					error={errors.name?.message}
+				/>
+				<Input
+					label='Ник'
+					type='text'
+					placeholder='Введите ник'
+					reg={{ ...register('nickname') }}
+					error={errors.nickname?.message}
+				/>
+				<Input
+					label='Почта'
+					type='email'
+					placeholder='Введите почту'
+					reg={{ ...register('email') }}
+					error={errors.email?.message}
+				/>
+				<div className='gender-div'>
+					<span className='gender-span'>Пол</span>
+					<Input
+						className='gender-input'
+						label='Мужчина:'
+						type='radio'
+						value='male'
+						reg={{ ...register('gender') }}
+					/>
+					<Input
+						className='gender-input'
+						label='Женщина:'
+						type='radio'
+						value='female'
+						{...register('gender')}
+					/>
+					{errors.gender && (
+						<span className='error'>{errors.gender.message}</span>
+					)}
+				</div>
+				<Input
+					label='Пароль'
+					type='password'
+					placeholder='Введите пароль'
+					reg={{ ...register('password') }}
+					error={errors.password?.message}
+				/>
+				<Input
+					label='Подтвердите пароль'
+					type='password'
+					placeholder='Подтвердите пароль'
+					reg={{ ...register('confirmPassword') }}
+					error={errors.confirmPassword?.message}
+				/>
+				<button type='submit'>Зарегистрироваться</button>
+			</form>
+		</div>
+	)
+}
+
+export const Signup = styled(SignupContainer)`
+	.gender-div {
+		display: flex;
+		align-items: start;
+		flex-direction: column;
+		margin-bottom: 0.2rem;
+	}
+
+	.gender-span {
+		margin-bottom: 0.5rem;
+	}
+
+	.gender-input {
+		flex-direction: row;
+		width: 100px;
+	}
+	button {
+		margin-top: 0.5rem;
+	}
+`
